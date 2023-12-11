74cd1825dff61a6afb4d340fc7ad76ff1d52bbc0
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2023-12-11 18:38:39 +0100

    Create home controller and added it to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  root "artists#index"
+  resources :artists
+
+  root "home#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
